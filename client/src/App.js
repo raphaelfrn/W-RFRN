a84b890f6d5c0a3e2607517a95f3e15fe140e1fd
Farina Raphael <raphael.farina@inetum.com>
2023-10-12 17:28:46 +0200

    init

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Characters from "./pages/Characters";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Characters/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update" element={<Update/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Characters.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+
+const Characters = () =>  {
+  const [characters,setCharacters] = useState([])
+
+  useEffect(() =>{
+    const fetchAllCharacters = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/characters");
+        setCharacters(res.data)
+        console.log(res)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllCharacters();
+    
+  }, []);
+  
+  console.log(characters);
+
+  return (
+    <div>
+
+        <h1>W-RFRN</h1>
+        <div className="characters">
+        {characters.map(character=>(
+          <div className="character" key={character.id}>
+              <h2>{character.character_name}</h2>
+              <h2>{character.character_lvl}</h2>
+              <h2>{character.character_class}</h2>
+
+          </div>
+        ))}
+        </div>
+        <button>
+          <Link to="/add">Add new character</Link>
+        </button>
+      
+      
+    </div>
+  )
+}
+
+export default Characters
